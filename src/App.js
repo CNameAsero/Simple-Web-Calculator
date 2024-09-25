5d4ef4f1a96a0550273d8b77d46f96a987698da8
CNameAsero <jonasagustin4212@gmail.com>
2024-09-25 18:53:29 +0800

    simple web calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export function Calculator() {
+  const [num, setNum] = useState("");
+
+  function numDisp(e) {
+    const targetNum = num + e.target.value;
+    setNum(targetNum);
+  }
+
+  function deletePrev() {
+    const delNum = num;
+    const removed = delNum.slice(0, -1)
+    setNum(removed);
+  }
+
+  function deleteAll() {
+    setNum("");
+  }
+
+  function finalValue() {
+    const processedNum = num.replace(/×/g, '*').replace(/÷/g, '/');
+    setNum(eval(processedNum));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="calcCont">
+        <form>
+          <div className='calcScreen'>
+            <input type="text" name="display" defaultValue={num}/>
+          </div>
+          <div>
+            <input type="button" value="AC" onClick={() => deleteAll()}/>
+            <input type="button" value="%" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="Del" onClick={() => deletePrev()}/>
+            <input type="button" value="&divide;" onClick={(e) => numDisp(e)}/>
+          </div>
+          <div>
+            <input type="button" value="7" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="8" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="9" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="&times;" onClick={(e) => numDisp(e)}/>
+          </div>
+          <div>
+            <input type="button" value="4" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="5" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="6" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="&#45;" onClick={(e) => numDisp(e)}/>
+          </div>
+          <div>
+            <input type="button" value="1" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="2" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="3" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="&#43;" onClick={(e) => numDisp(e)}/>
+          </div>
+          <div>
+            <input type="button" value="0" onClick={(e) => numDisp(e)}/>
+            <input type="button" value="." onClick={(e) => numDisp(e)}/>
+            <input type="button" value="&#61;" className='equals' onClick={() => finalValue()}/>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
